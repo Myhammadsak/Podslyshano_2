028042d53a5ab96da1eb263e3a1a05ab32d77c69
Холмухаммад <143716091+Myhammadsak@users.noreply.github.com>
2025-07-21 14:27:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 API_TOKEN = 'TOKEN'
 
 bot = telebot.TeleBot(API_TOKEN)
-anime = 0
+counter = 0
 
 
 @bot.message_handler(commands=['start'])
@@ -16,19 +16,17 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['post'])
 def send_welcome(message):
-    global anime
+    global counter
     send = bot.send_message(message.chat.id, 'Отправь сюда пост'.format(message.from_user))
-    anime = 1
+    counter = 1
     bot.register_next_step_handler(send, admin_message)
 
 @bot.message_handler(func=lambda message: True)
 def admin_message(message):
-    global anime
-    if anime == 1:
+    global counter
+    if counter == 1:
         bot.reply_to(message, 'Пост отправлен на рассмотрение!')
-        bot.forward_message(1420386760, message.chat.id, message.message_id)
-        bot.forward_message(1068456643, message.chat.id, message.message_id)
-        bot.copy_message(6460908058, message.chat.id, message.message_id)
+        bot.forward_message(ID_USER, message.chat.id, message.message_id)
         anime = 0
 
 
